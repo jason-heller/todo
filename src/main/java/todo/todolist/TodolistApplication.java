a11fa7e5f05b276f4765f88ddd7202d0358292e8
jheller9 <jasonheller96@gmail.com>
2021-06-25 16:35:18 -0400

    CRUD unit tests

--- src/main/java/todo/todolist/TodolistApplication.java
+++ src/main/java/todo/todolist/TodolistApplication.java
@@ -2,12 +2,16 @@ package todo.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import todo.todolist.controller.TodoTest;
 
 @SpringBootApplication
 public class TodolistApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TodolistApplication.class, args);
+
+		// SpringApplication.run(TodolistApplication.class, args);
+
+		new TodoTest();
 	}
 
 }
---
+++ src/main/java/todo/todolist/controller/TodoTest.java
@@ -0,0 +1,34 @@
+package todo.todolist.controller;
+
+import org.aspectj.lang.annotation.Before;
+import static org.mockito.Mockito.*;
+
+import todo.todolist.model.TodoItem;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TodoTest {
+    public TodoTest() {
+        TodoController cont = new TodoController();
+
+        TodoItem item1 = mock(TodoItem.class);
+        TodoItem item2 = mock(TodoItem.class);
+
+        when(item1.getId()).thenReturn(1l);
+        when(item1.getDescription()).thenReturn("item 1 - desc");
+        when(item1.getTitle()).thenReturn("item 1 - title");
+
+        when(item1.getId()).thenReturn(2l);
+        when(item1.getDescription()).thenReturn("item 2 - desc");
+        when(item1.getTitle()).thenReturn("item 2 - title");
+
+        // CRUD
+        cont.save(item1);
+        cont.update(1l, true);
+        cont.save(item2);
+        cont.find(false);
+        cont.findAll();
+        cont.delete(2l);
+        cont.findAll();
+    }
+}
